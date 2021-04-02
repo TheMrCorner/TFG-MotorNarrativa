7d196b093be2c17593174fddcb9538708586e48b
Carlos Gonzalez <gonzalem@ucm.es>
2021-04-02 14:58:17 +0200

    Added Story class

---
+++ Narrative_Engine/Narrative_Engine/Story.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Narrative_Engine
+{
+    public enum StoryType
+    {
+        MAIN,
+        SECONDARY
+    }
+    class Story
+    {
+        public readonly StoryType m_storyType;
+        // private List<Quest> m_quests;
+
+        public Story(StoryType type)
+        {
+            m_storyType = type;
+        }     
+    }
+}
