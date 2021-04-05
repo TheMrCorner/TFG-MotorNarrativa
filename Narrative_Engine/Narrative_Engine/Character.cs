ec018cc8b5c65b939595d1971d9773b1b8162fbf
Carlos Jiménez <carloj03@ucm.es>
2021-04-05 18:20:28 +0200

    Change Character position (coordinates) to a Place object, added setter for this parameter

--- Narrative_Engine/Narrative_Engine/Character.cs
+++ Narrative_Engine/Narrative_Engine/Character.cs
@@ -12,29 +12,30 @@ public class Character
         SECONDARYNPC,
         PLAYER
     }
-        
+
     private string characterName;
-    private KeyValuePair<float, float> position;
+    // private Place place;
     private CharacterType type;
     private Dictionary<Item.ItemType, List<Item>> inventory;
     // List<Dialog> dialogs; TODO
     // Other stats
 
-    public Character(string characterName, KeyValuePair<float, float> position, CharacterType type)
+    public Character(string characterName, Place place, CharacterType type)
     {
         this.characterName = characterName;
-        this.position = position;
+        this.place = place;
         this.type = type;
         inventory = new Dictionary<Item.ItemType, List<Item>>();
-        foreach(Item.ItemType itemType in Enum.GetValues(typeof(Item.ItemType)))
+        foreach (Item.ItemType itemType in Enum.GetValues(typeof(Item.ItemType)))
         {
             inventory.Add(itemType, new List<Item>());
         }
     }
 
     public string GetCharacterName() => characterName;
-    public KeyValuePair<float, float> GetPosition() => position;
+    public Place GetPlace() => place;
     public CharacterType GetCharacterType() => type;
+    public void SetPlace(Place place) => this.place = place;
     public void AddItemToInventory(Item item) => inventory[item.type].Add(item);
     public void RemoveItemFromInventory(Item item) => inventory[item.type].Remove(item);
 }
