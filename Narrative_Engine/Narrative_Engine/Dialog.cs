de393034931d6b125d0aaede76a895acf4ff6d62
Alejandro <alpermar98@gmail.com>
2021-04-08 11:48:28 +0200

    Dialogs and Scenes

---
+++ Narrative_Engine/Narrative_Engine/Dialog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Narrative_Engine
+{
+    public class Option
+    {
+        public int nextNode;
+        public string text;
+    } // Option
+
+    public class Node
+    {
+        public int character;
+        public int nextNode;
+        public string text;
+        public Option[] options;
+
+        public int GetCharacter()
+        {
+            return character;
+        } // GetCharacter
+
+        public int GetNextNode()
+        {
+            return nextNode;
+        } // GetNextNode
+
+        public int GetNumOptions()
+        {
+            return options.Length;
+        } // GetNumOptions
+    } // Node
+
+    class Dialog
+    {
+        public bool rewarded = true;
+        public Node[] nodeList; 
+
+        public bool GetRewarded()
+        {
+            return rewarded;
+        } // GetRewarded
+    } // Dialog
+} // namespace
