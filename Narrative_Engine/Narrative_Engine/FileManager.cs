9f3b46b64e54ca3e89af779c6d2145ade968dc28
Carlos Jiménez <carlosjimenez@xcjgames.com>
2021-07-24 19:42:33 +0200

    Pequeño fix para que Unity encuentre los archivos

--- Narrative_Engine/Narrative_Engine/FileManager.cs
+++ Narrative_Engine/Narrative_Engine/FileManager.cs
@@ -28,7 +28,12 @@ namespace Narrative_Engine
 
         public FileManager(string generalPath, string storyFolder, string chapterPath, string scenePath, string dialogFolder, string charactersPath, string itemsPath, string placesPath)
         {
-            m_generalPath = "../../../" + generalPath + "/";
+            #if DEBUG
+                m_generalPath = "../../../" + generalPath + "/";
+            #else
+                m_generalPath = generalPath + "/";
+            #endif
+
             m_storyFolder = m_generalPath + storyFolder;
             m_dialogFolder = m_generalPath + dialogFolder;
             m_charactersPath = m_generalPath + charactersPath;
